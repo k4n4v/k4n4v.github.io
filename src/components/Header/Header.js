77b99783ddaa005dbea465f3c7ae460e463a05dc
Kanav <kanav.12@hotmail.com>
2023-05-02 17:49:40 -0400

    added router and header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+import { Link, useLocation } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+
+  const getPage = () => {
+    const path = location.pathname;
+    const noSlash = path.replaceAll("/", "");
+    console.log(noSlash);
+    return noSlash === "" ? "About" : noSlash;
+  };
+
+  return (
+    <header>
+      <div className="titles">
+        <h1 className="name">
+          <Link to="/">Kanav Sharma</Link>
+        </h1>
+        <p className="title">{getPage()}</p>
+      </div>
+      <div className="nav-container">
+        <Link to="/">About</Link>
+      </div>
+    </header>
+  );
+}
